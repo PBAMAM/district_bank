a5facfe366d7715d6f757d11fda1ed446809915d
Astrit Marleku <astritmarleku1993@gmail.com>
2026-01-11 12:55:03 +0100

    More Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAyVyPv3T3hP52c9rCykYxhy9CX_SaHQkE",
-    authDomain: "bank-42b9b.firebaseapp.com",
-    projectId: "bank-42b9b",
-    storageBucket: "bank-42b9b.firebasestorage.app",
-    messagingSenderId: "479649049829",
-    appId: "1:479649049829:web:9e4d27598371d7b11ff80c",
-    measurementId: "G-BYXNFST17B"
-  }
+    apiKey: 'AIzaSyBau16vOW3TmhgD406KEZ914Ow76a1qxzY',
+    authDomain: 'swissone-durres.firebaseapp.com',
+    projectId: 'swissone-durres',
+    storageBucket: 'swissone-durres.firebasestorage.app',
+    messagingSenderId: '1080848573448',
+    appId: '1:1080848573448:web:391bf616e5feb714571dbd',
+    measurementId: 'G-WG8YKTHSH6',
+  },
 };
