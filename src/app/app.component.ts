3483864593c4276a3a2769667c05a49dcef7085f
Astrit Marleku <astritmarleku1993@gmail.com>
2026-03-11 21:03:35 +0100

    Wirking on Luxemburg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'HolaBank';
+  title = 'Luxembourg-Treasure';
   isAuthenticated = false;
 
   constructor(
